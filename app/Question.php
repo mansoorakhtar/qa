2d98541dfe7abb2aab8cc6b047b70090bb39baf0
Mansoor <mansoor.akhtar@zeptosystems.com>
2019-03-27 16:02:00 +0500

    Populate user and questions using database seed

--- app/Question.php
+++ app/Question.php
@@ -9,11 +9,17 @@ class Question extends Model
 
     protected $fillable = ['title', 'body'];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user() {
         return $this->belongsTo(User::class);
     }
 
-
+    /**
+     * @param string $value
+     * @return void
+     */
     public function setTitleAttribute($value) {
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = \Str::slug($value);
